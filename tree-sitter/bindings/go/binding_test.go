a323b8cdfcbd4ae33963a48e4cbbeffa38c3ee51
Sam Mohr <sam@sammohr.dev>
2026-05-19 14:31:12 -0400

    chore: add tree-sitter entries to .gitignore, fix go module path

--- tree-sitter/bindings/go/binding_test.go
+++ tree-sitter/bindings/go/binding_test.go
@@ -4,7 +4,7 @@ import (
 	"testing"
 
 	tree_sitter "github.com/tree-sitter/go-tree-sitter"
-	tree_sitter_camp "github.com/camp-language/camp/bindings/go"
+	tree_sitter_camp "github.com/camp-language/camp/tree-sitter/bindings/go"
 )
 
 func TestCanLoadGrammar(t *testing.T) {
---
+++ tree-sitter/bindings/go/go.mod
@@ -0,0 +1,5 @@
+module github.com/camp-language/camp/tree-sitter
+
+go 1.24.0
+
+require github.com/tree-sitter/go-tree-sitter v0.25.0
